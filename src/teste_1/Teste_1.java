2ebb77e732eaa750c4cb85f95d083897a0f62503
PC 08 <PC 08@192.168.55.117>
2019-10-08 19:23:47 -0300

    coloquei system

--- src/teste_1/Teste_1.java
+++ src/teste_1/Teste_1.java
@@ -16,6 +16,9 @@ public class Teste_1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ola Mundo");
+        
+      
     }
     
 }
